569dc0bf38f94f856a2925fd8e249cc745186ab2
4RomanR <4rprogramming@gmail.com>
2023-12-07 23:38:34 -0400

    Primer commit Rene Rivera

--- src/components/ReneRivera.jsx
+++ src/components/ReneRivera.jsx
@@ -1,6 +1,10 @@
 const ReneRivera = () => {
   return (
-    <div>ReneRivera</div>
+    <div>
+      <img src="./image/P_2023-11-14 2.jpeg" alt="René Rivera" className="w-60 mx-auto rounded-xl shadow-md text-center" />
+      <h2>René Rivera</h2>
+      <p>Fullstack programing stundent</p>
+    </div>
   )
 }
 export default ReneRivera
