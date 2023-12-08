3f862eda42fc96304ab5b905a2eff8367d5c9747
Dergi Samayoa <dergio@gmail.com>
2023-12-07 21:36:44 -0600

    Actualización de componente.

--- src/components/DergiSamayoa.jsx
+++ src/components/DergiSamayoa.jsx
@@ -1,6 +1,16 @@
 const DergiSamayoa = () => {
   return (
-    <div>DergiSamayoa</div>
+    <article className="text-center p-4 text-lg">
+        <header className="w-[230px] shadow-md rounded-md overflow-hidden mx-auto">
+            <img src="/images/foto_dergi.png" alt="Dergi Samayoa" />
+        </header>
+        <h2 className="font-bold">
+            DergiSamayoa
+        </h2>
+        <p className="text-slate-500 font-semibold">
+            Estudiando de React en Academlo
+        </p>
+    </article>
   )
 }
 export default DergiSamayoa
