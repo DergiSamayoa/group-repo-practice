864d9fc9f106f78840603a21ed032670be953a4a
Durlandy Blandón <durlandyb@gmail.com>
2023-12-07 22:36:58 -0500

    practica durlandy

--- src/components/DurlandyBlandon.jsx
+++ src/components/DurlandyBlandon.jsx
@@ -1,6 +1,14 @@
 const DurlandyBlandon = () => {
   return (
-    <div>DurlandyBlandon</div>
+    <article>
+   <header>
+    <img src="/images/durlandy.jpg" alt="" />
+   </header>
+   <h2>Durlandy Blandon Castaño</h2>
+   <p>
+
+   </p>
+    </article>
   )
 }
 export default DurlandyBlandon
